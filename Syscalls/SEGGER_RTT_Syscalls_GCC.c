34a119451decef1136d42dc28ed5a5d6d7bd3680
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2025-07-27 14:09:10 +0200

    Release V8.54

--- Syscalls/SEGGER_RTT_Syscalls_GCC.c
+++ Syscalls/SEGGER_RTT_Syscalls_GCC.c
@@ -3,7 +3,7 @@
 *                        The Embedded Experts                        *
 **********************************************************************
 *                                                                    *
-*            (c) 1995 - 2021 SEGGER Microcontroller GmbH             *
+*            (c) 1995 - 2019 SEGGER Microcontroller GmbH             *
 *                                                                    *
 *       www.segger.com     Support: support@segger.com               *
 *                                                                    *
@@ -41,11 +41,6 @@
 * DAMAGE.                                                            *
 *                                                                    *
 **********************************************************************
-*                                                                    *
-*       RTT version: 7.54                                           *
-*                                                                    *
-**********************************************************************
-
 ---------------------------END-OF-HEADER------------------------------
 File    : SEGGER_RTT_Syscalls_GCC.c
 Purpose : Low-level functions for using printf() via RTT in GCC.
